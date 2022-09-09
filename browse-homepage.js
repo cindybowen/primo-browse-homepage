08848c0d5c165cf27e632fb508b23917c035dd17
Cindy Bowen <75031778+cindybowen@users.noreply.github.com>
2022-09-09 10:19:53 -0400

    Update browse-homepage.js

--- browse-homepage.js
+++ browse-homepage.js
@@ -1,12 +1,12 @@
-/* function section may already exist in your custom.js, depending on other customizations, so just add LOCAL_VID portion to the section */
+/* function section may already exist in your custom.js, depending on other customizations, so just add LOCAL_VID portion to that section */
 
 (function () {
     "use strict";
     'use strict';
-  var LOCAL_VID = "YOUR_VIEW_HERE";
+  var LOCAL_VID = "YOUR_VIEW_CODE_HERE";
   
   
-/* Add Browse search 'home' page, with thanks to UIUC. NOTE: you also need to define LOCAL_VID (see above)  */
+/* Add Browse search 'home' page, with thanks to UIUC. NOTE: you need to define LOCAL_VID (see above)  */
       app.component('prmBrowseSearchAfter', {
         bindings: {parentCtrl: '<'},
         templateUrl: '/discovery/custom/' + LOCAL_VID + '/html/browse/browse_en.html'
